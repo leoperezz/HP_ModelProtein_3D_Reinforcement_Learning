aa39034f62d9c9c184977c7efb889eef49438d6e
Leonardo Pérez <83511434+leoperezz@users.noreply.github.com>
2023-01-21 13:39:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 import gc
+import math
 
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -49,8 +50,10 @@ action_n = env.action_space.n
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+N_ACTION = 5
+
 dqn = utils.DDQN(
-    output_shape=6,
+    output_shape=N_ACTION,
     device = device,
     sync_steps= args.sync_steps
 )
@@ -70,32 +73,39 @@ o = env.reset()
 o = dqn.preprocess_state(o)
 nepisode = 0
 
-eps_ = 1
-eps = eps_
+eps_max = 1
+eps_min = 0.01
 reward = 0
 game = 0
 reward_max = -np.inf
+l = 5
 
 
+max_frames = 1000
+frame = 0
 for t in tqdm(range(1, args.time_steps + 1)):
 
+    eps = max(eps_min,eps_min + (eps_max-eps_min)*math.e**(-t*l/args.time_steps))
     a = dqn.take_action(o,eps)
     o_, r, done, info = env.step(a)
     o_ = dqn.preprocess_state(o_)
     done = 1 if done else 0
     buffer.add(o, a, r, o_, done)
     reward += r
+    frame += 1
     if t >= args.warm_start and t % args.train_freq == 0:
         *transitions, idxs = buffer.sample(args.batch_size)
         priorities = dqn.train(*transitions,args.gamma)
         priorities = np.clip(np.abs(priorities), 1e-6, None)
         buffer.update_priorities(idxs, priorities)
-    if done:
+    if (done) | (frame == max_frames):
+        frame = 0
         o = env.reset()
         o = dqn.preprocess_state(o)
         writer.add_scalar("Reward",reward,global_step = game+1)
         game+=1
         if reward>=reward_max:
+            print("Saving model...")
             torch.save(dqn.Q.state_dict(),env_path)
             #ts_image = t/1e+6
             #path_img = f"saved_images/20merA/{args.sequence}_{ts_image}.fig.pickle" #change this every time that u want
@@ -108,6 +118,4 @@ for t in tqdm(range(1, args.time_steps + 1)):
         o = o_
 
     buffer.beta += (1 - args.beta) / args.time_steps
-
-    eps -= (eps_+0.3)/args.time_steps
  
