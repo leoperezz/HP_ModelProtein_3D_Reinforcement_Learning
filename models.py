9a129bdd7e01ed5ec5d983ad065fdeb48782c4e3
Leonardo Pérez <83511434+leoperezz@users.noreply.github.com>
2023-01-24 13:45:58 -0500

    Update models.py

--- models.py
+++ models.py
@@ -24,4 +24,31 @@ class LSTM_HP(nn.Module):
 
     return out_fc2
 
+  
+class LSTM_HP_ATT(nn.Module):
+
+  def __init__(self,input_size,hidden_size,fc_units,num_actions):
+    super(LSTM_HP_ATT,self).__init__()
+
+    self.input_size = input_size
+    self.hidden_size = hidden_size
+
+    self.lstm = nn.LSTM(input_size,hidden_size,batch_first=True)
+    self.att = nn.MultiheadAttention(hidden_size,8,batch_first=True)
+    self.lstm2 = nn.LSTM(hidden_size*2,hidden_size,batch_first = True)
+    self.fc1 = nn.Linear(hidden_size,fc_units)
+    self.fc2 = nn.Linear(fc_units,num_actions)
+    
+  def forward(self,x):
+    
+    out_lstm,_ = self.lstm(x)
+    att_out,_ = self.att(out_lstm,out_lstm,out_lstm)
+    att_out = torch.cat((out_lstm,att_out),dim=2)
+    out_lstm2,_= self.lstm2(att_out)
+
+    out_fc1 = self.fc1(out_lstm2[:,-1,:])
+    out_fc2 = self.fc2(out_fc1)
+
+
+    return out_fc2  
 
