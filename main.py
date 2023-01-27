04d47c339568d2bdaaa5154409539d3524f44441
Leonardo Pérez <83511434+leoperezz@users.noreply.github.com>
2023-01-27 09:02:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import gymnasium as gym
-from tqdm import tqdm
-import torch
-from torch import nn
-from torch.nn import functional as F
-from torch.optim.lr_scheduler import StepLR
-import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.tensorboard import SummaryWriter
-import gc
-import math
-
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore",category=UserWarning)
 
-from HPModel3D_env.envs.hpmodel_env import HPModel3D
+from tqdm import tqdm
+import torch
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
+import gc
+import math
+
+from HPModel3D_env.envs.hpmodel_env_v3_0 import HPModel3D
 import arguments
 import utils
 from sequences import sequences_dict
@@ -41,8 +36,8 @@ args=arguments.args
 
 steps_million = args.time_steps/1e+6
 
-best_model = f"saved_models/B{args.sequence}_{steps_million}M.pth"
-last_model = f"saved_models/L{args.sequence}_{steps_million}M.pth"
+best_model = f"saved_models/{args.environment}_B{args.num_model}_{args.sequence}_{steps_million}M.pth"
+last_model = f"saved_models/{args.environment}_L{args.num_model}_{args.sequence}_{steps_million}M.pth"
 
 env = HPModel3D(sequences_dict[args.sequence])
 
@@ -56,9 +51,9 @@ N_ACTION = 5
 dqn = utils.DDQN(
     output_shape=N_ACTION,
     device = device,
-    sync_steps= args.sync_steps
+    sync_steps= args.sync_steps,
+    model = args.num_model
 )
-
 
 buffer = utils.PrioritizedReplayBuffer(
     size = args.buffer_length,
@@ -66,8 +61,7 @@ buffer = utils.PrioritizedReplayBuffer(
     beta = args.beta
 )
 
-
-writer = SummaryWriter()
+writer = SummaryWriter(f'runs/{args.environment}_B{args.num_model}_{args.sequence}_{steps_million}M')
 o = env.reset()
 o = dqn.preprocess_state(o)
 nepisode = 0
@@ -77,7 +71,7 @@ eps_min = 0.01
 reward = 0
 game = 0
 reward_max = -np.inf
-l = 5
+l = 2.34
 
 
 max_frames = len(sequences_dict[args.sequence])*2
@@ -92,11 +86,13 @@ for t in tqdm(range(1, args.time_steps + 1)):
     buffer.add(o, a, r, o_, done)
     reward += r
     frame += 1
+
     if t >= args.warm_start and t % args.train_freq == 0:
         *transitions, idxs = buffer.sample(args.batch_size)
         priorities = dqn.train(*transitions,args.gamma)
         priorities = np.clip(np.abs(priorities), 1e-6, None)
         buffer.update_priorities(idxs, priorities)
+    
     if (done) | (frame == max_frames):
         frame = 0
         o = env.reset()
@@ -109,6 +105,7 @@ for t in tqdm(range(1, args.time_steps + 1)):
             print(f"reward :{reward_max}")
             torch.save(dqn.Q.state_dict(),best_model)
             reward_max = reward
+
         torch.save(dqn.Q.state_dict(),last_model)    
         reward=0
         gc.collect()
